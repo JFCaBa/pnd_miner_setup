e95395fe42eb0deaaa425caa09946a9058da57a7
Jose Catala <jfca68@gmail.com>
2024-01-24 13:57:06 +0300

    Fixed classes as files

--- miner_setup/main.py
+++ miner_setup/main.py
@@ -1,17 +1,17 @@
-
-import docker_manager
-import wifi_manager
-import connection_manager
-import notification_handler
-import bluetooth_service
+from docker_manager import DockerManager
+from wifi_manager import WifiManager
+from connection_manager import ConnectionManager
+from notification_handler import NotificationHandler
+from bluetooth_service import BluetoothService
 
 if __name__ == "__main__":
     wallet_address = None  # Initialize or retrieve from some source
-    docker_manager = docker_manager(wallet_address)
-    wifi_manager = wifi_manager()
-    connection_manager = connection_manager(wallet_address)
-    notification_handler = notification_handler(wallet_address)
-    bluetooth_service = bluetooth_service()
+
+    docker_manager_instance = DockerManager(wallet_address)
+    wifi_manager_instance = WifiManager()
+    connection_manager_instance = ConnectionManager(wallet_address)
+    notification_handler_instance = NotificationHandler(wallet_address)
+    bluetooth_service_instance = BluetoothService()
 
     # Run your Bluetooth service
-    bluetooth_service.run()
+    bluetooth_service_instance.run()
