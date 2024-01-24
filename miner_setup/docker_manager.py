3abbf14087ebbbd6f1d363fe13914819ea92902d
Jose Catala <jfca68@gmail.com>
2024-01-24 13:44:00 +0300

    fixed tab in function

--- miner_setup/docker_manager.py
+++ miner_setup/docker_manager.py
@@ -5,7 +5,7 @@ class DockerManager:
     def __init__(self, wallet_address):
         self.wallet_address = wallet_address
 
-def start_docker_container(self):
+    def start_docker_container(self):
         # Stop the existing container if it's running
         subprocess.run(["docker", "stop", "worker"], stderr=subprocess.DEVNULL)
         subprocess.run(["docker", "rm", "worker"], stderr=subprocess.DEVNULL)
